e05831320423c616c32a88c92defa06abed82724
Niels Robben <niels.robben@nl.abnamro.com>
2020-04-10 17:00:18 +0200

    Added action

--- lib/fastlane/plugin/niels/actions/niels_action.rb
+++ lib/fastlane/plugin/niels/actions/niels_action.rb
@@ -7,19 +7,20 @@ module Fastlane
       def self.run(params)
         require 'rqrcode'
 
-      qrcode = RQRCode::QRCode.new("http://github.com/")
+        qrcode = RQRCode::QRCode.new("http://github.com/")
 
-      # NOTE: showing with default options specified explicitly
-      svg = qrcode.as_svg(
-        offset: 0,
-        color: '000',
-        shape_rendering: 'crispEdges',
-        module_size: 6,
-        standalone: true
-      )
+        # NOTE: showing with default options specified explicitly
+        svg = qrcode.as_svg(
+          offset: 0,
+          color: '000',
+          shape_rendering: 'crispEdges',
+          module_size: 6,
+          standalone: true
+        )
 
-      puts svg
-end
+        puts svg
+        
+      end
 
 puts result
       end
